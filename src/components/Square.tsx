d973be56bed120ebedaccafa12f5f1418f0ac189
Daniil Timerkhanov <user565656@yandex.ru>
2025-05-01 23:48:30 +0500

    change color to yellow

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -8,7 +8,7 @@ const Square = ({ value, setSquareValue }: SquareProps) => {
         <button
             onClick={setSquareValue}
             style = {{
-                color: value ? '#e642f5' : '#282c34',
+                color: value ? '#e3f542' : '#282c34',
             }}
         >
             {value || '-'}
